50cd68781bd2c71f3521b297bbb6aa627a1acfd5
Anastasiia-Bilenko <122826970+Anastasiia-Bilenko@users.noreply.github.com>
2023-07-11 06:19:36 +0000

    Deploying to gh-pages from @ Anastasiia-Bilenko/goit-js-hw-09@425d5367bfe97cb0f7901ec8fc8d0d0fb5b7b16b 🚀

---
+++ 01-color-switcher.a13abf4e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(d){n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(a){clearInterval(n),t.disabled=!1,e.disabled=!0}));var n=null;console.dir(a)}();
+//# sourceMappingURL=01-color-switcher.a13abf4e.js.map
